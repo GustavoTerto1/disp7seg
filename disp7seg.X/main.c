e661bff39791734775e243675aa14ccf5aa52b0b
20185147 <20185147@DSN1271158608.cfp127aluno.local>
2021-02-26 16:51:30 -0300

    botao adicionado

--- disp7seg.X/main.c
+++ disp7seg.X/main.c
@@ -17,20 +17,31 @@ void main(void)
     int cont = 0;
     display7seg_int();
     botao_init();
-    
+    char estado = 0;
     while(1)
     {
-        switch(estado )
+        switch (estado )
         {
             case 0:
                 if( s1 () == 1 )
-        }           estado = 1;
-        if( s1() == 1 )
-        {
-        display (cont++ ); 
-        
+                   estado = 1;
+                break;
+                
+            case 1:    
+                    if (s1 () == 0 )
+                        estado = 2;
+                    break;
+                    
+            case 2:
+                    ++cont;
+                    estado = 0;
+                    break;
         }        
+        
+        
         if( cont >= 16 )
             cont = 0;
+        if( cont< 0 )
+            cont = 9;
     }   
 }
